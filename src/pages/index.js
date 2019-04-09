27c9826a0cbe3438809cb67eade0b742fa75a502
tarek <tarek.demachkie@gmail.com>
2019-04-09 14:32:52 -0500

    finishes pricing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -143,30 +143,30 @@ class IndexPage extends Component {
           <div className="photo-display__column">
             {shuffledEdges.slice(0, size).map(({ node }, i)=> {
               if([4].includes(i)) return (<div id={`photo-${i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}>
-                  <Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.3) rotate(-3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/>
+                  <Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.1) rotate(-3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/>
                 </div>)
-              if([11].includes(i)) return (<div id={`photo-${i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.3) rotate(3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
+              if([11].includes(i)) return (<div id={`photo-${i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.1) rotate(3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
               return (<div id={`photo-${i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px'}}/></div>)
             })}  
           </div>
           <div className="photo-display__column">
             {shuffledEdges.slice(size, (size + size)).map(({ node }, i)=> {
-              if([6].includes(i)) return (<div id={`photo-${size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.3) rotate(3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
-              if([10].includes(i)) return (<div id={`photo-${size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.3) rotate(-3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
+              if([6].includes(i)) return (<div id={`photo-${size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.1) rotate(3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
+              if([10].includes(i)) return (<div id={`photo-${size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.1) rotate(-3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
                 return (<div id={`photo-${size + i}`} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px'}}/></div>)
               })} 
           </div>
           <div className="photo-display__column">
             {shuffledEdges.slice((size + size), (size + size + size)).map(({ node }, i)=> {
-              if([1].includes(i)) return (<div id={`photo-${size + size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.3) rotate(3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
-              if([4].includes(i)) return (<div id={`photo-${size + size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.3) rotate(-3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
+              if([1].includes(i)) return (<div id={`photo-${size + size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.1) rotate(3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
+              if([4].includes(i)) return (<div id={`photo-${size + size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.1) rotate(-3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
                   return (<div id={`photo-${size + size + i}`} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px'}}/></div>)
             })} 
           </div>
           <div className="photo-display__column">
             {shuffledEdges.slice((size + size + size), (size + size + size + size)).map(({ node }, i)=> {
-              if([6].includes(i)) return (<div id={`photo-${size + size + size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.3) rotate(3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
-              if([11].includes(i)) return (<div id={`photo-${size + size + size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.3) rotate(-3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
+              if([6].includes(i)) return (<div id={`photo-${size + size + size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.1) rotate(3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
+              if([11].includes(i)) return (<div id={`photo-${size + size + size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px', transform: 'scale(1.1) rotate(-3deg)', zIndex: 2, boxShadow: '0px 0px 21px -3px black'}}/></div>)
                     return (<div id={`photo-${size + size + size + i}`} key={node.id} className={`photo-container`} data-id={node.id} onClick={this.openImageViewer}><Img fluid={node.file.image.fluid} style={{marginBottom: '14px'}}/></div>)
             })}
           </div>
--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -12,19 +12,53 @@ export default ({ data }) => (
             </div>
             <div className='price-blocks__container'>
                 <div className="price-block">
-                    <h3 className="block-title">BASIC</h3>
-                    <div className="block-body"></div>
-                    <a></a>
+                    <div>
+                        <h3 className="block-title">BASIC</h3>
+                        <div className="block-body">
+                            <ul className="plan-list">
+                                <li className="plan-list__item">6 hours coverage</li>
+                                <li className="plan-list__item">1 photographer</li>
+                                <li className="plan-list__item">200 edited pictures</li>
+                                <li className="plan-list__item">~ 500 total pictures</li>
+                                <li className="plan-list__item">flash drive delivery</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a className="plan-price">$ 999</a>
+                </div>
+                <div className="price-block special">
+                    <p className="special-tag">Most Popular</p>
+                    <div>
+                        <h3 className="block-title">PREMIUM</h3>
+                        <div className="block-body">
+                            <ul className="plan-list">
+                                <li className="plan-list__item">8 hours coverage</li>
+                                <li className="plan-list__item">1 photographer</li>
+                                <li className="plan-list__item">400 edited pictures</li>
+                                <li className="plan-list__item">~ 800 total pictures</li>
+                                <li className="plan-list__item">flash drive delivery</li>
+                                <li className="plan-list__item">online gallery</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a className="plan-price">$ 1,400</a>
                 </div>
                 <div className="price-block">
-                    <h3 className="block-title">PREMIUM</h3>
-                    <div className="block-body"></div>
-                    <a></a>
-                </div>
-                <div className="price-block">
-                    <h3 className="block-title">GOLDEN</h3>
-                    <div className="block-body"></div>
-                    <a></a>
+                    <div>
+                        <h3 className="block-title">GOLDEN</h3>
+                        <div className="block-body">
+                            <ul className="plan-list">
+                                <li className="plan-list__item">8 hours coverage</li>
+                                <li className="plan-list__item">2 photographers</li>
+                                <li className="plan-list__item">600 edited pictures</li>
+                                <li className="plan-list__item">~ 1100 total pictures</li>
+                                <li className="plan-list__item">flash drive delivery</li>
+                                <li className="plan-list__item">online gallery</li>
+                                <li className="plan-list__item">photo Album</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <a className="plan-price">$ 2,100</a>
                 </div>
             </div>
         </div>
